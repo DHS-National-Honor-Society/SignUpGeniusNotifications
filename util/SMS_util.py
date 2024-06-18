5e35cbb5b5416a2b3404d15ee10e06be6a64aae9
carson7005 <carson.chan2007@gmail.com>
2024-06-17 23:33:18 -0400

    Phone numbers can now be extracted directly

--- util/SMS_util.py
+++ util/SMS_util.py
@@ -1,12 +1,70 @@
 import requests as r
-from util import config_util as cutil, \
+import config_util as cutil, \
 log_util as lutil
-import json
+
+import os.path
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+from typing import List
+
+def getPhoneArray() -> List[List[str]]:
+
+
+  # If modifying these scopes, delete the file token.json.
+  SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
+
+# The ID of a sample document.
+  SHEETS_ID = cutil.get_config_item("sheet_id")
+
+
+
+  creds = None
+  # The file token.json stores the user's access and refresh tokens, and is
+  # created automatically when the authorization flow completes for the first
+  # time.
+  if os.path.exists("sheet_token.json"):
+    creds = Credentials.from_authorized_user_file("sheet_token.json", SCOPES)
+  # If there are no (valid) credentials available, let the user log in.
+  if not creds or not creds.valid:
+    if creds and creds.expired and creds.refresh_token:
+      creds.refresh(Request())
+    else:
+      flow = InstalledAppFlow.from_client_secrets_file(
+          r"sheet_credentials.json", SCOPES
+      )
+      creds = flow.run_local_server(port=0)
+    # Save the credentials for the next run
+    with open("sheet_token.json", "w") as token:
+      token.write(creds.to_json())
+
+  try:
+    array = []
+    service = build("sheets", "v4", credentials=creds)
+
+    # Retrieve the documents contents from the Docs service.
+    sheets = service.spreadsheets()
+    result = sheets.values().get(spreadsheetId=SHEETS_ID, range="B:D").execute()
+    
+    values = result.get("values", [])
+
+
+    for i in range(len(values)-1):
+      array.append(values[i+1])
+    
+    return array
+  except HttpError as err:
+    print(err)
+
+
+
+
 def sendSMSreminder(body, recipient):
     phoneNum = getPhone(recipient)
-  
     response = r.post("https://textbelt.com/text", {
-    "phone":"7348813725",
+    "phone":phoneNum[0],
     "message":body,
     "key": cutil.get_config_item("textbelt_key")
   })
@@ -14,6 +72,7 @@ def sendSMSreminder(body, recipient):
 
 
 def getPhone(name: str):
-  with open("student_numbers.json","r") as file:
-    numbers = json.load(file)
-    return [i['phone'] for i in file if i['name'] == name]
+  phones = getPhoneArray()
+  
+  return [i[2] for i in phones if f"{i[0]} {i[1]}".lower() == name.lower()]
+  
